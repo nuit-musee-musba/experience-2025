826fd3a932d2ede4d1d7fe47017d4d9be782fc99
Safirl <loic.leforestier@mmibordeaux.com>
2025-01-13 15:46:45 +0100

    default position

--- src/experiences/2-reserve/js/exhibitionScene.js
+++ src/experiences/2-reserve/js/exhibitionScene.js
@@ -10,12 +10,16 @@ export default class ExhibitionScene extends Scene {
         this.perspectiveRotation = 1920;
         this.paintingPositions = [
             {
-                x: 0,
-                y: 0,
+                x: 3104,
+                y: 864,
             },
             {
-                x: 1,
-                y: 0,
+                x: 2618,
+                y: 578,
+            },
+            {
+                x: 2044,
+                y: 533,
             }
         ]
     }
@@ -45,9 +49,25 @@ export default class ExhibitionScene extends Scene {
     rotatePainting(painting) {
         if (parseInt(painting.style.left, 10) > this.perspectiveRotation) {
             painting.style.transform = "skew(0deg, 30deg)";
-        }
-        else {
+        } else {
             painting.style.transform = "skew(0deg, -30deg)";
+        }
+    }
+
+    calculateDistance(x1, y1, x2, y2) {
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    //Change the inPos to a default one if close enough
+    GetClosestPos(inPos) {
+        for (const pos of this.paintingPositions) {
+            if (this.calculateDistance(inPos.x, inPos.y, pos.x, pos.y) < 300) {
+                inPos.x = pos.x;
+                inPos.y = pos.y;
+                return;
+            }
         }
     }
 
@@ -55,19 +75,24 @@ export default class ExhibitionScene extends Scene {
         super.initScene();
         this.fetchPaintings();
         document.addEventListener("click", (event) => {
-            let painting = document.querySelector("#selected-painting").children[0];
-
-            const paintingWidth = painting.offsetWidth;
-            const paintingHeight = painting.offsetHeight;
-
-            const centeredLeft = event.clientX - paintingWidth / 2;
-            const centeredTop = event.clientY - paintingHeight / 2;
-
-            painting.style.left = `${centeredLeft}px`;
-            painting.style.top = `${centeredTop}px`;
-
-            this.rotatePainting(painting);
+            this.setPaintingPosition({x: event.clientX, y: event.clientY});
         });
+    }
+
+    setPaintingPosition(pos) {
+        let painting = document.querySelector("#selected-painting").children[0];
+        
+        this.GetClosestPos(pos)
+        const paintingWidth = painting.offsetWidth;
+        const paintingHeight = painting.offsetHeight;
+
+        const centeredLeft = pos.x - paintingWidth / 2;
+        const centeredTop = pos.y - paintingHeight / 2;
+
+        painting.style.left = `${centeredLeft}px`;
+        painting.style.top = `${centeredTop}px`;
+
+        this.rotatePainting(painting);
     }
 
     unloadScene() {
