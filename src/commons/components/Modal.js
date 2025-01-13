57de1531c47f6f1ebac13d06116e6951abfd0f97
ccadran <clar.cadran@gmail.com>
2025-01-13 11:08:47 +0100

    feat(setting): add basic styles & create modal component

---
+++ src/commons/components/Modal.js
@@ -0,0 +1,51 @@
+
+
+export class Modal {
+    constructor(className, title, content) {
+      this.className = className;
+      this.title = title;
+      this.content = content;
+  
+
+      this.modal = document.createElement('div');
+      this.modal.className = `modal ${this.className}`;
+  
+
+      this.titleElement = document.createElement('h3');
+      this.titleElement.className = 'h3-title-serif';
+      this.titleElement.textContent = this.title;
+  
+
+      this.contentElement = document.createElement('p');
+      this.contentElement.textContent = this.content;
+
+      this.line = document.createElement('div')
+      this.line.className = 'line'
+
+  
+      this.modal.appendChild(this.titleElement);
+      this.modal.appendChild(this.line);
+      this.modal.appendChild(this.contentElement);
+
+      document.body.appendChild(this.modal);
+    }
+
+
+    show() {
+        this.modal.style.display = 'flex';
+    }
+
+
+    close() {
+        this.modal.style.display = 'none';
+    }
+
+    remove() {
+        this.modal.remove();
+    }
+
+    changeContent(title, content) {
+        this.titleElement.textContent = title;
+        this.contentElement.textContent = content;
+    }
+  }
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Modal } from "./commons/components/Modal";
+
+const modal = new Modal('modal', 'Modal Title', 'This is the modal content Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ex praesentium quo nam molestiae earum, cum consequuntur? Illo accusantium harum quo voluptas ipsam obcaecati suscipit ea fugit, ducimus alias quis.');
+
+modal.show();
+
+
