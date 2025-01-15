9495e787c2cb595ede2cbc4e8c3c380809df35fd
Héloïse P. <heloise.pingitore@mmibordeaux.com>
2025-01-15 17:22:30 +0100

    add css

--- src/experiences/2-reserve/js/paintingChoice.js
+++ src/experiences/2-reserve/js/paintingChoice.js
@@ -1,3 +1,5 @@
+import { Modal } from "../../../commons/components/Modal";
+import { descriptionPaintings } from "./descriptionPainting";
 import paintings from "../data/paintings";
 import selectedPaintings from "../data/selectedPaintings";
 import Game from "./Game";
@@ -6,7 +8,6 @@ import AudioManager from "./audioManager";
 
 
 let containerPaintings = document.querySelector('#container-paintings');
-let sceneReserve = document.querySelector('#scene-reserve');
 
 containerPaintings.addEventListener("click", (e) => {
     if (e.target.tagName !== "IMG") {
@@ -26,28 +27,33 @@ containerPaintings.addEventListener("click", (e) => {
 });
 
 function addbackgroundAndDescription(img, parentImg, paintingData) {
-    let title = document.createElement('p');
-    let autor = document.createElement('p');
-    let date = document.createElement('p');
-    let description = document.createElement('p');
-    let height = document.createElement('p');
-    let descriptionContainer = document.createElement('div');
+    let descriptionContainer = document.createElement('div'); 
+    let containerButton = document.createElement('div')
+    const modal = new descriptionPaintings(paintingData.title, "", "selectModeDescription", descriptionContainer);
+    const contentArray = [
+        ` ${paintingData.autor}`,
+        `${paintingData.date}`,
+        `${paintingData.description}`,
+        `${paintingData.height}`
+    ];
+    modal.showModalWithHtml(contentArray);
 
-    title.innerText = paintingData.title;
-    autor.innerText = paintingData.autor;
-    date.innerText = paintingData.date;
-    description.innerText = paintingData.description;
-    height.innerText = paintingData.height;
 
-    descriptionContainer.append(title, autor, date, description, height);
+   
+
     parentImg.classList.add('selectMode');
+    img.classList.add("selectModeImg");
+    parentImg.classList.add('selectMode');
+    containerButton.classList.add('containerButton')
     img.classList.add("selectModeImg");
     descriptionContainer.classList.add('descriptionContainer');
 
     let removeLink = createRemoveLink(parentImg, img);
     let pushLink = createPushLink(paintingData, parentImg, img);
 
-    descriptionContainer.append(removeLink, pushLink);
+
+    descriptionContainer.append(removeLink, pushLink, containerButton);
+    containerButton.append(removeLink, pushLink)
     parentImg.append(descriptionContainer);
 }
 
@@ -64,6 +70,7 @@ function removeBackgroundAndDescription(parentImg, img) {
 function createRemoveLink(parentImg, img) {
     let removeLink = document.createElement('a');
     removeLink.href = "#";
+    removeLink.classList.add('small', 'black', 'nextButton' ,'button')
     removeLink.innerText = "Retour";
     removeLink.style.marginRight = "10px";
     removeLink.addEventListener("click", (e) => {
@@ -76,6 +83,7 @@ function createRemoveLink(parentImg, img) {
 function createPushLink(paintingData, parentImg, img) {
     let pushLink = document.createElement('a');
     pushLink.href = "#";
+    pushLink.classList.add('small', 'black', 'nextButton' ,'button')
     pushLink.innerText = "Valider";
     pushLink.addEventListener("click", (e) => {
         e.preventDefault();
