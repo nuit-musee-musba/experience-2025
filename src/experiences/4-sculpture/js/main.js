ca52c2fb327da65aa56d00faff79b3305ab929a3
Ton Nom <matthiascasanova0311@gmail.com>
2025-01-16 10:31:34 +0100

    fix inventaire

--- src/experiences/4-sculpture/js/main.js
+++ src/experiences/4-sculpture/js/main.js
@@ -2,25 +2,19 @@ import DragDrop from './DragDrop.js';
 import Item from './Item.js';
 import Dialogue from './Dialogue.js';
 import Step from './currentStep.js';
-import { Button } from '../../../commons/components/Button.js';
 
-const stepManager = new Step(1); // Initialisation de l'étape avec Step
-console.log(`Étape initialisée : ${stepManager.getCurrentStep()}`);
-
+const stepManager = new Step(1);
 const inventorySlots = document.querySelector('.inventory-slots');
 
-// Crée les items pour les identifiants 1 à 9
+// Crée les items
 for (let item = 1; item <= 6; item++) {
-    const itemInstance = new Item(item); // Passe l'identifiant de l'item
-    inventorySlots.appendChild(itemInstance.element); // Ajoute l'élément à l'inventaire
-    console.log(`Item ajouté : ${item}`);
+    const itemInstance = new Item(item);
+    inventorySlots.appendChild(itemInstance.element);
 }
 
-// Initialiser le Drag & Drop avec `stepManager` pour gérer les étapes
+// Initialiser le Drag & Drop
 new DragDrop('#work-area', '.item', stepManager);
 
 // Initialiser les dialogues
 const dialogue = new Dialogue();
 dialogue.listDialogue(Array.from({ length: 30 }, (_, i) => i), '#dialogue-element');
-
-const btn = new Button()
