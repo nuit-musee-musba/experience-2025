af0d50c7325f0e6600a69ef1d62dbea7943a0f30
Safirl <loic.leforestier@mmibordeaux.com>
2025-01-16 16:55:46 +0100

    changed event handler

--- src/experiences/2-reserve/data/elements.js
+++ src/experiences/2-reserve/data/elements.js
@@ -9,8 +9,8 @@ export default [
     {
         id: 0,
         src: "./assets/img/elements/lianes.png",
-        x: 951,
-        y: 71,
+        x: 0,
+        y: 0,
         thematic: "nature",
     },
 ]
