11701f22b369417844e3b577a1f6d3230a82183c
Safirl <loic.leforestier@mmibordeaux.com>
2025-01-16 09:59:19 +0100

    remove unused comments

--- src/experiences/2-reserve/data/selectedElements.js
+++ src/experiences/2-reserve/data/selectedElements.js
@@ -1,9 +1,2 @@
 export default [
-    // {
-    //     id: 0,
-    //     src: "./assets/img/elements/lianes.png",
-    //     x: 951,
-    //     y: 71,
-    //     thematic: "nature",
-    // },
 ]
