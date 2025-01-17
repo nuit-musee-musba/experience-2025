a2865cd10fa1ee070b768a1372ca1f00ae183106
Safirl <loic.leforestier@mmibordeaux.com>
2025-01-17 15:51:11 +0100

    remove defaults

--- src/experiences/2-reserve/data/selectedElements.js
+++ src/experiences/2-reserve/data/selectedElements.js
@@ -1,16 +1,2 @@
 export default [
-    {
-        id: 0,
-        src: "./assets/img/elements/vitrail.png",
-        x: 0,
-        y: 0,
-        thematic: "paix",
-    },
-    {
-        id: 0,
-        src: "./assets/img/elements/lianes.png",
-        x: 0,
-        y: 0,
-        thematic: "nature",
-    },
 ]
