46e5142819bfc1298f339f6687dcba1e5413227f
Sakushi <116188905+ImSakushi@users.noreply.github.com>
2025-01-18 22:40:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,9 @@ import * as fs from "fs";
 import { join, resolve } from "path";
 import { defineConfig } from "vite";
 
-
 // Définir les répertoires
 const root = resolve(__dirname, "src");
-
 const outDir = resolve(__dirname, "dist");
-
 
 const redirectToDir = ({ root }) => ({
   name: "redirect-to-dir",
@@ -34,24 +31,34 @@ export default defineConfig({
   root,
   publicDir: "../public/",
   plugins: [redirectToDir({ root })],
-  base: "./", 
+  base: "./",
   build: {
     outDir,
     emptyOutDir: true,
-    sourcemap: true, 
-
-
+    sourcemap: true,
     rollupOptions: {
       input: {
-
+        // Pages principales
         "root": "./src/index.html",
-
         "1-hub": "./src/experiences/1-hub/index.html",
         "2-reserve": "./src/experiences/2-reserve/index.html",
         "3-techniques": "./src/experiences/3-techniques/index.html",
         "3-techniques/game": "./src/experiences/3-techniques/game.html",
         "4-sculpture": "./src/experiences/4-sculpture/index.html",
         "5-peintures": "./src/experiences/5-peintures/index.html",
+        "5-peintures/accueil-baroque": "./src/experiences/5-peintures/accueil-baroque.html",
+        "5-peintures/accueil-renaissance": "./src/experiences/5-peintures/accueil-renaissance.html",
+        "5-peintures/accueil-romantisme": "./src/experiences/5-peintures/accueil-romantisme.html",
+        "5-peintures/accueil-symbolisme": "./src/experiences/5-peintures/accueil-symbolisme.html",
+        "5-peintures/baroque": "./src/experiences/5-peintures/baroque.html",
+        "5-peintures/end-game": "./src/experiences/5-peintures/end-game.html",
+        "5-peintures/renaissance": "./src/experiences/5-peintures/renaissance.html",
+        "5-peintures/romantisme": "./src/experiences/5-peintures/romantisme.html",
+        "5-peintures/start-baroque": "./src/experiences/5-peintures/start-baroque.html",
+        "5-peintures/start-renaissance": "./src/experiences/5-peintures/start-renaissance.html",
+        "5-peintures/start-romantisme": "./src/experiences/5-peintures/start-romantisme.html",
+        "5-peintures/start-symbolisme": "./src/experiences/5-peintures/start-symbolisme.html",
+        "5-peintures/symbolisme": "./src/experiences/5-peintures/symbolisme.html",
         "6-restaurations": "./src/experiences/6-restaurations/index.html",
       },
     },
